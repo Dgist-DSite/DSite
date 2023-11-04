39a17d5c0e06b90b8cb72e91e735a24b4afaa7b5
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-11-05 01:33:39 +0900

    Feat :: 예외처리 추가

--- src/main/java/DSite/service/BoardServiceImpl.java
+++ src/main/java/DSite/service/BoardServiceImpl.java
@@ -3,9 +3,16 @@ package DSite.service;
 import DSite.dto.BoardDto;
 import DSite.entity.BoardEntity;
 import DSite.repository.BoardRepository;
+import DSite.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -17,41 +24,113 @@ public class BoardServiceImpl implements BoardService{
 
 
     @Override
-    public void create(BoardDto boardDto) {
-        System.out.println(boardDto.getContent());
+    public ResponseEntity<BaseResponse> create(BoardDto boardDto) throws IOException {
+        Document document = Jsoup.connect(boardDto.getUrl()).get();
+        String ogTitle = document.select("meta[property=og:title]").attr("content"); //제목
+        String ogImage = document.select("meta[property=og:image]").attr("content"); //이미지
+        String ogDescription = document.select("meta[property=og:description]").attr("content"); //설명
+        boardDto.setTitle(ogTitle);
+        boardDto.setImage(ogImage);
+        boardDto.setDescription(ogDescription);
         BoardEntity boardEntity = dtoToEntity(boardDto);
-        boardRepository.save(boardEntity);
-       // System.out.println(boardEntity.getCategory() + boardEntity.getContent() + boardEntity.getUrl());
+
+        BaseResponse baseResponse = new BaseResponse();
+
+        try {
+            boardRepository.save(boardEntity);
+            baseResponse.setStatus(HttpStatus.CREATED);
+            baseResponse.setMessage("게시글 작성 성공");
+            return ResponseEntity.ok().body(baseResponse);
+        } catch (DataAccessException e) {
+            baseResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            baseResponse.setMessage(e.getMessage());
+            return ResponseEntity.badRequest().body(baseResponse);
+        }
+
+
+
     }
 
     @Override
-    public Map<String , List<BoardDto>> getAllContent() {
-        List<BoardEntity> boardEntities = boardRepository.findAll();
-        List<BoardDto> boardDtos = boardEntities.stream()
-                .map(this::entityToDto)
-                .collect(Collectors.toList());
+    public ResponseEntity<BaseResponse> getContents(Long id) {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            Optional<BoardEntity> boardEntity= boardRepository.findById(id);
+            if (boardEntity.isPresent()) {
+                baseResponse.setData(boardEntity.get());
+                baseResponse.setMessage("Find");
+                baseResponse.setStatus(HttpStatus.OK);
+                return ResponseEntity.ok(baseResponse);
+            } else {
+                baseResponse.setStatus(HttpStatus.NOT_FOUND);
+                baseResponse.setMessage("Board id not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponse);
+            }
+        } catch (Exception e) {
+            baseResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            baseResponse.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(baseResponse);
+        }
 
-        //역순으로 데이터 반환
-        Collections.reverse(boardDtos);
-
-        Map<String, List<BoardDto>> resultMap = new HashMap<>();
-        resultMap.put("data", boardDtos);
-
-        return resultMap;
     }
 
     @Override
-    public Map<String, List<BoardDto>> getCategoryContent(String category) {
-       List<BoardEntity> boardEntity = boardRepository.findByCategoryContaining(category);
-       List<BoardDto> boardDtos = boardEntity.stream()
-               .map(this::entityToDto)
-               .collect(Collectors.toList());
-       Collections.reverse(boardDtos);
+    public ResponseEntity<BaseResponse> getAllContents() {
+        BaseResponse baseResponse = new BaseResponse();
+        try{
+            List<BoardEntity> boardEntities = boardRepository.findAll();
+            if (!boardEntities.isEmpty()){
+                List<BoardDto> boardDtos = boardEntities.stream()  // 코드 중복성 이거 나중에 함수로 빼서 리펙토링
+                        .map(this::entityToDto)
+                        .collect(Collectors.toList());
 
-       Map<String,List<BoardDto>> resultMap = new HashMap<>();
-        resultMap.put("data", boardDtos);
+                //역순으로 데이터 반환
+                Collections.reverse(boardDtos);
 
-       return resultMap;
+                baseResponse.setStatus(HttpStatus.OK);
+                baseResponse.setMessage("성공");
+                baseResponse.setData(boardDtos);
+                return ResponseEntity.ok(baseResponse);
+            }
+            else {
+                baseResponse.setStatus(HttpStatus.NOT_FOUND);
+                baseResponse.setMessage("NOT_FOUND");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponse);
+            }
+
+        } catch (Exception e){
+            baseResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            baseResponse.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(baseResponse);
+        }
+    }
+
+    @Override
+    public ResponseEntity<BaseResponse> getCategoryContents(String category) {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            List<BoardEntity> boardEntity = boardRepository.findByCategoryContaining(category);
+            if (!boardEntity.isEmpty()){
+                List<BoardDto> boardDtos = boardEntity.stream()
+                        .map(this::entityToDto)
+                        .collect(Collectors.toList());
+                Collections.reverse(boardDtos);
+
+                baseResponse.setStatus(HttpStatus.OK);
+                baseResponse.setMessage("성공");
+                baseResponse.setData(boardDtos);
+                return ResponseEntity.ok(baseResponse);
+            }
+            else {
+                baseResponse.setMessage(HttpStatus.NOT_FOUND);
+                baseResponse.setMessage("NOT_FOUND");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(baseResponse);
+            }
+        } catch (Exception e){
+            baseResponse.setMessage(e.getMessage());
+            baseResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(baseResponse);
+        }
     }
 
 
