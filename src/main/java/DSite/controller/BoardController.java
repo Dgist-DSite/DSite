66d62cbf2de9251f5563d7e35e70d6bea3248e53
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-11-05 18:04:54 +0900

    Fix :: CORS 오류 수정

--- src/main/java/DSite/controller/BoardController.java
+++ src/main/java/DSite/controller/BoardController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class BoardController {
 
     private final BoardService boardService;
